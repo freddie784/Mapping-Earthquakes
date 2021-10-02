9a9b99e82c8f925771075f7c952e6c577c53ed3d
freddie784 <83510059+freddie784@users.noreply.github.com>
2021-10-02 17:22:41 -0400

    added map layers

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,20 +1,30 @@
 console.log("working");
 
-//Create map object with a center and zoom level
-let map = L.map('mapid').setView([30, 30], 2);
-
-
-
-
-
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: apiKey
 });
 
-//add our 'graymap' tile layer
-streets.addTo(map);
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: apiKey
+});
+
+let baseMaps = {
+    Street : streets,
+    Dark : dark
+};
+//Create map object with a center and zoom level
+let map = L.map('mapid', {
+    center : [40.7,-94.5],
+    zoom : 4,
+    layers : [streets]
+});
+
+L.control.layers(baseMaps).addTo(map);
+
 
 let airportData = "https://raw.githubusercontent.com/freddie784/Mapping-Earthquakes/main/majorAirports.json";
 
