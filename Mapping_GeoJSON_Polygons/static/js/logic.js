93314f1044d80c850db030a4f50874eea4848ed8
freddie784 <83510059+freddie784@users.noreply.github.com>
2021-10-02 18:16:20 -0400

    created polygons showing neighborhoods in Toronto

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,44 +1,45 @@
 console.log("working");
 
-let day = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: apiKey
 });
 
-let night = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: apiKey
 });
 
 let baseMaps = {
-    Day : day,
-    Night : night
+    Streets : streets,
+    "Satellite Streets" : satellite
 };
 //Create map object with a center and zoom level
 let map = L.map('mapid', {
-    center : [44.0,-80.0],
-    zoom : 2,
-    layers : [day]
+    center : [43.7,-79.3],
+    zoom : 11,
+    layers : [streets]
 });
 
 L.control.layers(baseMaps).addTo(map);
 
-let torontoData = "https://raw.githubusercontent.com/freddie784/Mapping-Earthquakes/main/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/freddie784/Mapping-Earthquakes/main/torontoNeighborhoods.json";
 
 let myStyle = {
-    color : "lightYellow",
-    weight : 2
+    color : "blue",
+    weight : 1,
+    fillColor : "yellow"
 }
 
-d3.json(torontoData).then(data => {
+d3.json(torontoHoods).then(data => {
     console.log(data);
     // Creating a GeoJson Layer with data
     L.geoJson(data, {
-        style : myStyle,
-        onEachFeature: function(feature, layer) {
-          layer.bindPopup("<h2> Airline: " + feature.properties.airline + "<hr></hr>" + "<h4> Destination: " + feature.properties.dst + "</h4>");  
+        style: myStyle,
+        onEachFeature : function(feature, layer) {
+            layer.bindPopup("<h2> Neighborhood: " + feature.properties.AREA_NAME + "</h2>");
         }
     }).addTo(map);
 
